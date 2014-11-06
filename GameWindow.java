f1012ab8d7af9e226569f45e91637f1ee8a54a2c
Ryan Pierce <rdpierce285@gmail.com>
2014-11-05 20:55:17 -0500

    X-Men: First Class
    Added the first class.

---
+++ GameWindow.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author Monkey
+ *
+ */
+public class GameWindow extends JFrame{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
